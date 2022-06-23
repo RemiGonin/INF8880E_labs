0df7dd2615e2e59a35da23182a4ccba79adfe2c3
Remi Gonin <gonin.r50@gmail.com>
2022-06-23 11:28:16 -0400

    colorscale units and style

--- Projet/src/map.py
+++ Projet/src/map.py
@@ -18,6 +18,11 @@ def add_choro_trace(fig_map, regions_data, map_df, dfs_map, category):
 
     '''
 
+    if category == "Alcoholic drinks" or category == "Soft drinks":
+        unit = "ml"
+    else:
+        unit = "g"
+
     figure = go.Choroplethmapbox(
         geojson=regions_data,
         locations=map_df.index,
@@ -28,17 +33,17 @@ def add_choro_trace(fig_map, regions_data, map_df, dfs_map, category):
         reversescale=True,
         marker_line_color="white",
         zmid=dfs_map["moy2019"].loc[category][0],
-        # colorbar=dict(thicknessmode='pixels',
-        #               thickness=0,
-        #               borderwidth=0,
-        #               outlinewidth=0,
-        #               showticklabels=False,
-        #               ypad=0,
-        #               xpad=0,
-        #               tickwidth=0,
-        #               ticklen=0,
-        #               len=0
-        #               )
+        zmax=map_df[category].max(),
+        zmin=map_df[category].min(),
+        colorbar=dict(title=unit+"/pers/week",
+                      titleside="top",
+                      tickmode="array",
+                      tickvals=[map_df[category].min(
+                      ), dfs_map["moy2019"].loc[category][0], map_df[category].max()],
+                      ticktext=[str(map_df[category].min()), str(
+                          dfs_map["moy2019"].loc[category][0]) + " National average", str(map_df[category].max())],
+                      ticks="outside"
+                      )
     )
 
     fig_map.add_trace(figure)
