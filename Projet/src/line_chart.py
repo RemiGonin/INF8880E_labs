3e5a45c9e17a25bf5ca37d1e535d3d1ebf3144b8
dysoxor <asobolev.pro@gmail.com>
2022-06-23 17:49:51 +0300

    progress line

--- Projet/src/line_chart.py
+++ Projet/src/line_chart.py
@@ -2,4 +2,12 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 def add_line_trace(fig, data):
+    x = data.columns.to_list()[1:]
+    for i in range(data.shape[0]):
+        fig.add_trace(go.Scatter(x=x, y=data.iloc[[i]][data.columns.to_list()[1:]].reset_index(drop=True).values.flatten().tolist(), name=data.iloc[[i]].index.to_list()[0],
+                         line=dict(width=3)))
+    fig.update_layout(
+        plot_bgcolor = "white",
+        paper_bgcolor = "white"
+        )
     return fig
